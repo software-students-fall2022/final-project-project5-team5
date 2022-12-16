c07b949d8d0b9189b461dd6956ee420cef264470
cwj2099 <wc1629@nyu.edu>
2022-12-16 17:39:06 -0500

    trying to fix

--- web-app/tests/test_routes.py
+++ web-app/tests/test_routes.py
@@ -7,16 +7,16 @@ def test_home(flask_app):
     assert response.status_code == 200
 
 def test_category(flask_app):
-    response = flask_app.get('/')
+    response = flask_app.get('/category/')
     assert response.request.path == "/category/"
     assert response.status_code == 200
 
 def test_search(flask_app):
-    response = flask_app.get('/')
+    response = flask_app.get('/search')
     assert response.request.path == "/search"
     assert response.status_code == 200
 
 def test_delete(flask_app):
-    response = flask_app.get('/')
+    response = flask_app.get('/delete')
     assert response.request.path == "/delete"
     assert response.status_code == 200
